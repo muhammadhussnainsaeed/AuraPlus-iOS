1d5e1fc017f19eca0ca0bb764df3ba0579df0327
muhammadhussnainsaeed <m.hussnainsaeed@gmail.com>
2025-03-17 18:05:57 +0500

    Initial Commit

---
+++ Auraplus/Components/MessageInputView.swift
@@ -0,0 +1,18 @@
+//
+//  MessageInputView.swift
+//  Auraplus
+//
+//  Created by Hussnain on 22/3/25.
+//
+
+import SwiftUI
+
+struct MessageInputView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MessageInputView()
+}
---
+++ Auraplus/Views/ChatRoomView.swift
@@ -0,0 +1,18 @@
+//
+//  ChatRoomView.swift
+//  Auraplus
+//
+//  Created by Hussnain on 22/3/25.
+//
+
+import SwiftUI
+
+struct ChatRoomView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ChatRoomView()
+}
